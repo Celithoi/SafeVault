223b67f51035e78c5669358cf65ce5d49a27cac0
Marcelo Henrique Rocha Cardoso <mhrcardoso@gmail.com>
2026-06-10 11:22:53 -0300

    feat: create application layouts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import { PublicLayout } from "../layouts/PublicLayout";
+
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>SafeVault</h1>,
+    element: <PublicLayout />,
+    children: [
+      {
+        index: true,
+        element: <h1>SafeVault</h1>,
+      },
+    ],
   },
 ]);

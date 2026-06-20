d6f8b8fc96682bb40beed2796bae341a6441fa9c
Marcelo Henrique Rocha Cardoso <mhrcardoso@gmail.com>
2026-06-20 12:54:51 -0300

    feat: implement sidebar navigation and connect landing page to app

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
+
 export function SideBar() {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/app">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/app/folders">Folders</Link>
+          </li>
+          <li>
+            <Link to="/app/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/app/settings">Settings</Link>
+          </li>
+          <li>
+            <Link to="/login">Sair (Simulação)</Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
